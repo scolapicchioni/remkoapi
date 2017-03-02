ee93820eb865423c8b7b59336274aa4b666cc111
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-03-02 09:41:37 +0100

    First

---
+++ src/RemkoApi/Controllers/RemkoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RemkoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class RemkoController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// I WAS TRYING THIS BUT IT DOES NOT WORK. YOU DON'T NEED THIS GUY
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]object value)
+        {
+            var bodyStream = this.Request.Body;
+            
+            var requestBody = new StreamReader(bodyStream).ReadToEnd();
+
+            Console.WriteLine(value.ToString());
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/RemkoApi/RemkoMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace RemkoApi
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class RemkoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RemkoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var bodyStream = context.Request.Body;
+
+            var requestBody = new StreamReader(bodyStream).ReadToEnd();
+            Console.WriteLine(requestBody);
+
+            await _next(context);
+
+            await context.Response.WriteAsync("OH HI!");
+            
+            
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class RemkoMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRemkoMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RemkoMiddleware>();
+        }
+    }
+}
