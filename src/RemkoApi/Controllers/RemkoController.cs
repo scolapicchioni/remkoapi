3d8da092baa0b678e07284805aeb88f12f219926
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-03-09 12:50:23 +0100

    Changed Get In RemkoController

--- src/RemkoApi/Controllers/RemkoController.cs
+++ src/RemkoApi/Controllers/RemkoController.cs
@@ -16,7 +16,7 @@ namespace RemkoApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "version 01", "starter" };
         }
 
         // GET api/values/5
